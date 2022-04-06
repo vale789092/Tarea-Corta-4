ddce03711c888b0e1ef67a59acdb83e78323787b
vale789092 <valeska2110@estudiantec.cr>
2022-04-06 13:49:37 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Botones boton = new Botones();
+    }
+    //El orden correcto de los botones es:
+    // - ROJO
+    // - BLANCO
+    // - AZUL
+}
